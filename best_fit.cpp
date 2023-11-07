106be2615675bd6d61775f3ab75e375f8f08d2e5
coder0719 <150162322+coder0719@users.noreply.github.com>
2023-11-08 00:36:40 +0530

    Update best_fit.cpp

--- best_fit.cpp
+++ best_fit.cpp
@@ -1,47 +1,53 @@
 #include <iostream>
 using namespace std;
 
-#define max 25
-
 int main() {
-    int frag[max], b[max], f[max], i, j, nb, nf, temp, lowest = 10000;
-    static int bf[max], ff[max];
+    int frag[25], b[25], f[25], bf[25], ff[25];
+    int nb, nf;
 
-    cout << "\nEnter the number of blocks:";
+    cout << "Enter the number of blocks: ";
     cin >> nb;
-    cout << "Enter the number of files:";
+    cout << "Enter the number of files: ";
     cin >> nf;
 
-    cout << "\nEnter the size of the blocks:-\n";
-    for (i = 1; i <= nb; i++) {
-        cout << "Block " << i << ":";
+    cout << "Enter the size of the blocks:\n";
+    for (int i = 1; i <= nb; i++) {
+        cout << "Block " << i << ": ";
         cin >> b[i];
+        bf[i] = 0; 
     }
 
-    cout << "Enter the size of the files :-\n";
-    for (i = 1; i <= nf; i++) {
-        cout << "File " << i << ":";
+    cout << "Enter the size of the files:\n";
+    for (int i = 1; i <= nf; i++) {
+        cout << "File " << i << ": ";
         cin >> f[i];
+        ff[i] = 0; 
     }
 
-    for (i = 1; i <= nf; i++) {
-        for (j = 1; j <= nb; j++) {
-            if (bf[j] != 1) {
-                temp = b[j] - f[i];
-                if (temp >= 0 && lowest > temp) {
-                    ff[i] = j;
-                    lowest = temp;
-                }
+    for (int i = 1; i <= nf; i++) {
+        int bestFitBlock = -1;
+        int smallestBlockSize = 25;
+
+        for (int j = 1; j <= nb; j++) {
+            if (bf[j] == 0 && b[j] >= f[i] && b[j] < smallestBlockSize) {
+                bestFitBlock = j;
+                smallestBlockSize = b[j];
             }
         }
-        frag[i] = lowest;
-        bf[ff[i]] = 1;
-        lowest = 10000;
+
+        if (bestFitBlock != -1) {
+            ff[i] = bestFitBlock;
+            bf[bestFitBlock] = 1; // Mark the block as allocated
+            frag[i] = b[bestFitBlock] - f[i];
+        }
     }
 
-    cout << "\nFile No\tFile Size \tBlock No\tBlock Size\tFragment";
-    for (i = 1; i <= nf && ff[i] != 0; i++)
-        cout << "\n" << i << "\t\t" << f[i] << "\t\t" << ff[i] << "\t\t" << b[ff[i]] << "\t\t" << frag[i];
+    cout << "\nFile No\tFile Size\tBlock No\tBlock Size\tFragment" << endl;
+    for (int i = 1; i <= nf; i++) {
+        if (ff[i] != 0) {
+            cout << i << "\t\t" << f[i] << "\t\t" << ff[i] << "\t\t" << b[ff[i]] << "\t\t" << frag[i] << endl;
+        }
+    }
 
     return 0;
 }
